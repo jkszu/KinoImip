4082efd905f8cfaf939ce0f954d6664928fbaceb
jksz <jaksz@yandex.com>
2020-01-20 03:24:01 +0100

    Further DI config: register assembly

--- KinoImip/KinoImipCore/ContainerConfig.cs
+++ KinoImip/KinoImipCore/ContainerConfig.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace KinoImipCore
@@ -10,6 +12,10 @@ namespace KinoImipCore
         public static IContainer Configure()
         {
             var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(KinoImipLibrary)))
+                .Where(t => t.Namespace.Contains("Interfaces"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
 
             return builder.Build();
         }
