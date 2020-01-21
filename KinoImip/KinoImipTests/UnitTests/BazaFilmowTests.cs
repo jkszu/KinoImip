0edefaaba7b807c44c37aab1691b980730831528
wojszel <wojszel@yandex.com>
2020-01-21 21:51:25 +0100

    Further tests work

--- KinoImip/KinoImipTests/UnitTests/BazaFilmowTests.cs
+++ KinoImip/KinoImipTests/UnitTests/BazaFilmowTests.cs
@@ -15,8 +15,22 @@ namespace KinoImipTests.UnitTests
         public void ZwrocFilmyTest()
         {
             // Arrange
+            var baza = new BazaFilmow();
+
+            var film1 = new Film("The Lighthouse", 2019);
+            var film2 = new Film("1917", 2020);
+            var filmyExpected = new List<IFilm>();
+            filmyExpected.Add(film1);
+            filmyExpected.Add(film2);
+
+            baza.DodajFilm(film1);
+            baza.DodajFilm(film2);
+
             // Act
+            var answer = baza.ZwrocFilmyTest();
+
             // Assert
+            Assert.That(answer, Is.EqualTo(filmyExpected), "ZwrocFilmy() failure");
         }
     }
 }
