10830f2865506919c384a2f151a2c9bfb3724429
michswit <michswit@yandex.com>
2020-01-22 02:36:32 +0100

    Tests and abstraction for Kierownik class

--- KinoImip/KinoImipLibrary/Model/Kierownik.cs
+++ KinoImip/KinoImipLibrary/Model/Kierownik.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using KinoImipLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KinoImipLibrary.Model
 {
-    public class Kierownik
+    public class Kierownik : IKierownik
     {
+        public void DodajFilm(IFilm film)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DodajSeans(ISeans seans)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrzegladajFilmy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrzegladajSeanse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UsunFilm(IFilm film)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UsunSeans(ISeans seans)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- KinoImip/KinoImipTests/UnitTests/KierownikTests.cs
+++ KinoImip/KinoImipTests/UnitTests/KierownikTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using KinoImipLibrary.Interfaces;
+using KinoImipLibrary.Model;
+using KinoImipLibrary.Services;
+using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,13 +14,28 @@ namespace KinoImipTests.UnitTests
     [TestFixture]
     public class KierownikTests
     {
+        IKierownik kierownik;
+        BazaFilmow bazaFilmow = BazaFilmow.Instance;
+        BazaSeansow bazaSeansow = BazaSeansow.Instance;
+
+        [SetUp]
+        public void Init()
+        {
+            kierownik = new Kierownik();
+        }
+
         [Test]
         public void DodajSeansTest()
         {
             // Arrange
+            var film1 = new Film("The Lighthouse", 2019);
+            var seans1 = new Seans(1, film1);
 
             // Act
+            Kierownik.DodajSeans(seans1);
+
             // Assert
+            Assert.That(bazaSeansow.Seanse.Contains(seans1), Is.True, "DodajSeans() failure");
         }
 
         [Test]
@@ -58,6 +76,13 @@ namespace KinoImipTests.UnitTests
             // Arrange
             // Act
             // Assert
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            bazaFilmow.Filmy.Clear();
+            bazaSeansow.Seanse.Clear();
         }
     }
 }
